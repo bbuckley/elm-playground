ceff4cfd53d107716e18f931fec215c2d1b31f8e
brian buckley <briankbuckley@gmail.com>
2019-09-14 15:02:19 -0400

    futzing

--- src/Main.elm
+++ src/Main.elm
@@ -10,12 +10,12 @@ import Html.Attributes exposing (src)
 
 
 type alias Model =
-    {}
+    { n : Int }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { n = 8 }, Cmd.none )
 
 
 
@@ -42,6 +42,7 @@ view model =
         , h1 [] [ text "Your Elm App is working. This is a UI test." ]
         , h3 [] [ text "Your Elm App is working. This is a UI test." ]
         , img [ src "/logo.svg" ] []
+        , h3 [] [ text (String.fromInt model.n) ]
         ]
 
 
