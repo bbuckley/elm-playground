e6669ad5c4a0efd1cf8885d0bf6cd2a6cb3a2a68
brian buckley <briankbuckley@gmail.com>
2019-09-16 21:44:23 -0400

    works!

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, h3, img, input, text)
+import Html.Attributes exposing (placeholder, src, type_, value)
+import Html.Events exposing (onInput)
 
 
 
@@ -10,12 +11,22 @@ import Html.Attributes exposing (src)
 
 
 type alias Model =
-    {}
+    { numeratorString : String
+    , denominatorString : String
+    , numerator : Float
+    , denominator : Float
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { numeratorString = "1"
+      , denominatorString = "2"
+      , numerator = 1
+      , denominator = 2
+      }
+    , Cmd.none
+    )
 
 
 
@@ -23,12 +34,28 @@ init =
 
 
 type Msg
-    = NoOp
+    = NumeratorInput String
+    | DenominatorInput String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NumeratorInput s ->
+            ( { model
+                | numeratorString = s
+                , numerator = Maybe.withDefault model.numerator (String.toFloat s)
+              }
+            , Cmd.none
+            )
+
+        DenominatorInput s ->
+            ( { model
+                | denominatorString = s
+                , denominator = Maybe.withDefault model.denominator (String.toFloat s)
+              }
+            , Cmd.none
+            )
 
 
 
@@ -38,8 +65,13 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working" ]
+        [ h1 [] [ text ("numeratorString is " ++ model.numeratorString) ]
+        , h1 [] [ text ("denominatorString is " ++ model.denominatorString) ]
+        , h1 [] [ text ("numerator is " ++ String.fromFloat model.numerator) ]
+        , h1 [] [ text ("denominator is " ++ String.fromFloat model.denominator) ]
+        , h1 [] [ text ("numerator / denominator is " ++ String.fromFloat (model.numerator / model.denominator)) ]
+        , h3 [] [ input [ type_ "text", placeholder "enter numerator", value model.numeratorString, onInput NumeratorInput ] [] ]
+        , h3 [] [ input [ type_ "text", placeholder "enter denominator", value model.denominatorString, onInput DenominatorInput ] [] ]
         ]
 
 
