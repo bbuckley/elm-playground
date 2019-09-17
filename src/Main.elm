a990e64b8f354b04e562237ee28630b220d9500b
brian buckley <briankbuckley@gmail.com>
2019-09-17 09:40:20 -0400

    add color

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, h1, h3, img, input, text)
-import Html.Attributes exposing (placeholder, src, type_, value)
+import Color exposing (Color, red)
+import Html exposing (Html, div, h1, h3, img, input, span, text)
+import Html.Attributes exposing (placeholder, src, style, type_, value)
 import Html.Events exposing (onInput)
 
 
@@ -65,9 +66,9 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text ("numeratorString is " ++ model.numeratorString) ]
-        , h1 [] [ text ("denominatorString is " ++ model.denominatorString) ]
+        [ h1 [] [ text "numeratorString is ", span [ style "color" "red" ] [ text model.numeratorString ] ]
         , h1 [] [ text ("numerator is " ++ String.fromFloat model.numerator) ]
+        , h1 [] [ text ("denominatorString is " ++ model.denominatorString) ]
         , h1 [] [ text ("denominator is " ++ String.fromFloat model.denominator) ]
         , h1 [] [ text ("numerator / denominator is " ++ String.fromFloat (model.numerator / model.denominator)) ]
         , h3 [] [ input [ type_ "text", placeholder "enter numerator", value model.numeratorString, onInput NumeratorInput ] [] ]
