2349655247711d2019e3786171f500b315bbdeb8
Brian Buckley <briankbuckley@gmail.com>
2019-10-05 17:10:41 -0400

    dont know

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (Html, div, h1, hr, p, text)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (id, style)
 import Html.Events exposing (onClick)
 import Random
 
@@ -12,12 +12,14 @@ import Random
 
 
 type alias Model =
-    List Float
+    { list : List Float
+    , new : Maybe Float
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( [], Cmd.none )
+    ( Model [] Nothing, Cmd.none )
 
 
 
@@ -34,13 +36,29 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         DelRandomNumber f ->
-            ( model |> List.filter ((/=) f), Cmd.none )
+            ( { model
+                | list = model.list |> List.filter ((/=) f)
+                , new =
+                    if List.all ((/=) f) model.list then
+                        -- model.new
+                        Nothing
+
+                    else
+                        Nothing
+              }
+            , Cmd.none
+            )
 
         AddRandomNumber ->
             ( model, Random.generate Roll (Random.float 1.8 2) )
 
         Roll float ->
-            ( float :: model, Cmd.none )
+            ( { model
+                | list = float :: model.list
+                , new = Just float
+              }
+            , Cmd.none
+            )
 
 
 
@@ -52,16 +70,53 @@ s =
     [ style "cursor" "pointer", style "border" "solid" ]
 
 
+xxx : Float -> Maybe Float -> Bool
+xxx fl mf =
+    case mf of
+        Nothing ->
+            False
+
+        Just f ->
+            fl == f
+
+
+mark_id : Model -> List (Html Msg)
+
+
+
+-- mark_id : Model -> List (Float, Bool)
+-- mark_id : Model -> List (List (Html.Attribute Msg))
+
+
+mark_id model =
+    List.map
+        (\f ->
+            if xxx f model.new then
+                -- (f, True)
+                p (id "new" :: (onClick (DelRandomNumber f) :: s)) [ String.fromFloat f |> text ]
+
+            else
+                -- (f, False)
+                p (onClick (DelRandomNumber f) :: s) [ String.fromFloat f |> text ]
+        )
+        model.list
+
+
 view : Model -> Html Msg
 view model =
     div []
         [ h1 [ onClick AddRandomNumber, style "cursor" "pointer" ] [ text "Your Elm App is working - click to add rand" ]
         , hr [] []
-        , div [][div [] (List.map (\f -> p (onClick (DelRandomNumber f) :: s) [ String.fromFloat f |> text ]) model)]
+        , div []
+            (mark_id model)
         ]
 
 
 
+-- pp f model =
+--   case model.new of
+--      Just fl -> (onClick (DelRandomNumber fl) :: s)
+--      Nothing -> (onClick (DelRandomNumber f) :: s)
 ---- PROGRAM ----
 
 
